cf031da5e85f117a103b4cd7ea7dff93363767d1
Aiden Bohlander <99673331+TheTridentGuy@users.noreply.github.com>
2025-04-12 20:52:20 -0700

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,18 @@
+import board
+import digitalio
+import pwmio
+import storage
+import time
+from config import BRIGHTNESS
+
+button1 = digitalio.DigitalInOut(board.GP27)
+button1.direction = digitalio.Direction.INPUT
+button2 = digitalio.DigitalInOut(board.GP26)
+button2.direction = digitalio.Direction.INPUT
+
+if not button1.value and not button2.value:
+    storage.disable_usb_drive()
+else:
+    red_pwm1 = pwmio.PWMOut(board.GP21, frequency=100, duty_cycle=65535-(BRIGHTNESS * 65535))
+    red_pwm2 = pwmio.PWMOut(board.GP18, frequency=100, duty_cycle=65535-(BRIGHTNESS * 65535))
+    time.sleep(1)
